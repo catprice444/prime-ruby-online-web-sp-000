2ca3f57e355328815545ff0f48d0646aa61ef787
Catherine Price <catprice444@gmail.com>
2020-05-02 17:13:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,17 @@
 # require 'prime'
 
-def prime(n)
-  if 
+def prime?(n)
+  if n <= 1 || n == 0 || n == 1
+    return false
+  elsif
+    (2..n - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+    end
+  end
+  true
+end
+
+
+  

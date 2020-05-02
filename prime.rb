c5e372669c29fd1527d10fb4b397e47cd063fa76
Catherine Price <catprice444@gmail.com>
2020-05-02 17:05:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 require 'prime'
 
-def prime_number(number)
-  array = []
-  (array).to_a
-  Prime.each(number) do |prime|
-  end
-end
+# def prime_number(number)
+#   array = []
+#   (array).to_a
+#   Prime.each(number) do |prime|
+#   end
+# end
+
+prime? = []
+Prime.prime?()
